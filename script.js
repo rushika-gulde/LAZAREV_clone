4c277fa76e09e6babe487622e547f28a4986fa32
Rushika Gulde <rushikagulde@gmail.com>
2025-06-15 19:17:49 -0400

    Updated project

--- script.js
+++ script.js
@@ -66,3 +66,10 @@ video.addEventListener("click",function(){
     
 })
 
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('navLinks');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
+
